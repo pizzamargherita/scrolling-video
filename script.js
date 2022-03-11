3e3db1a5f568dd0e9c6c53d64d040851900356d2
adrin <pizzamargueritame@gmail.com>
2022-03-11 13:16:48 -0500

    CARDS

--- script.js
+++ script.js
@@ -134,12 +134,4 @@ setInterval(function() {
   r(sec, 6 * d.getSeconds())
   r(min, 6 * d.getMinutes())
   r(hour, 30 * (d.getHours() % 12) + d.getMinutes() / 2)
-}, 1000)
-
-/*STOP SCROLL*/
-$(window).scroll(function() {
-  if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-    clearTimeout(scrolldelay);
-    alert("end!");
-  }
-});
+}, 1000);
